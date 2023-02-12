2f7e12aeab1c44b5a8abf012a736151fd8f543da
William Chalk <3halk1@gmail.com>
2023-02-12 13:25:01 -0500

    data file creation and csv file creation

---
+++ Util.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCSV(List<Employee> employees)
+        {
+            if (!Directory.Exists("data"))
+            {
+                //if not create data folder
+                Directory.CreateDirectory("data");
+            }
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
+        }
+    }
+}
